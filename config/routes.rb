cf9770da77eb8426b5b0e76a5e91cf0026b12ffe
sintapi2030 <matsushitataku15@gmail.com>
2020-07-24 10:18:31 +0900

    sign_up-mark_up_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
-Rails.application.routes.draw do
-  devise_for :users
+Rails.application.routes.draw do 
+  devise_for :users, controllers: {seessions: 'users/sessions', registrations: 'users/registrations'}
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+    get 'accounts', to: 'users/registrations#new_account'
+    post 'accounts', to: 'users/registrations#new_account'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "items#index"
   resources :items, only: [:index]
