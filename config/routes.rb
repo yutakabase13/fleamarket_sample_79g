230bf0cbf5ff3d90ac8bcdc5b3823021dc32e148
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-07-24 15:48:05 +0900

    Revert "ログインページのビュー"

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "items#index"
   resources :items, only: [:index]
-  resources :homes, only: [:new]
 end
