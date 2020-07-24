7c9058df26bbfb4ef3cd975d14f44c2962b6f9ce
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-07-24 10:41:20 +0900

    respec blank test at first_name

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :account do
     first_name {"名前"}
-    last_name {"苗字"}
+    family_name {"苗字"}
     hurigana_first {"なまえ"}
     hurigana_family {"みょうじ"}
     birthday {Faker::Date.birthday}
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -7,5 +7,14 @@ RSpec.describe Account, type: :model do
         expect(build(:account)).to be_valid
       end
     end
+    context 'can not save' do
+      context 'with blank' do
+        it 'is invalid without first_name' do
+          account = build(:account, first_name: nil)
+          account.valid?
+          expect(account.errors[:first_name]).to include('を入力してください')
+        end
+      end
+    end
   end
 end
