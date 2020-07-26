af20ca23f8876eef0095afdae5e94676664dd20a
namatame-karaage <mariypuuu0519@gmail.com>
2020-07-26 17:46:23 +0900

    fix/gitgnore and Reason for comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :basic_auth, if: :production?
-  # before_action :authenticate_user!
+  # before_action :authenticate_user!　
+  #強制的にuser_sign_inに行ってしまい、ビューが確認できないため
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
