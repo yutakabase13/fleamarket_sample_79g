514fa0714381b7d72e4d4500a0b7a779408a006b
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-07-26 18:09:06 +0900

    add reason for comment out section

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :basic_auth, if: :production?
-  # before_action :authenticate_user!
+  # before_action :authenticate_user!　ビュー編集のため一時的にコメントアウト
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
