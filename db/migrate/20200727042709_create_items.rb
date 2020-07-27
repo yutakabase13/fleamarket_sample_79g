9ac74f9b3a857b97e9def041a162e27906dd0833
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-07-27 13:48:49 +0900

    add model

---
+++ db/migrate/20200727042709_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name, null: false
+      t.text :description, null: false
+      t.string :item_status, null: false
+      t.integer :shipping_fee, null: false
+      t.string :owner_area, null: false
+      t.integer :shipping_date, null: false
+      t.integer :seller_id, index: true
+      t.integer :buyer_id, index: true
+      # t.references :category, foreign_key: true
+      t.references :brand, foreign_key: true
+      t.timestamps
+    end
+  end
+end
