67781f9eec5e99a8d8a45775e4932b4a9bab06d5
sintapi2030 <matsushitataku15@gmail.com>
2020-07-28 11:23:50 +0900

    product-markup_commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
-  before_action :basic_auth, if: :production?
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :basic_auth
+  # before_action :basic_auth, if: :production?
+  # before_action :authenticate_user!
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "items#index"
-  resources :items, only: [:index]
+  resources :items, only: [:index] 
+  get 'items/sell'
 end
