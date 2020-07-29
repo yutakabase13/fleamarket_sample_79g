23184f40df559689e9bfe5223743c174884351f3
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-07-29 16:00:10 +0900

    add references to item

--- db/migrate/20200727042709_create_items.rb
+++ db/migrate/20200727042709_create_items.rb
@@ -10,7 +10,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string :shipping_date, null: false
       t.integer :seller_id, index: true
       t.integer :buyer_id, index: true
-      # t.references :category, foreign_key: true
+      t.references :category, foreign_key: true
       t.references :brand, foreign_key: true
       t.timestamps
     end
