1b9807c20256d66b48967b652db034821310101c
namatame-karaage <mariypuuu0519@gmail.com>
2020-07-29 19:02:04 +0900

    fix/migrationfile seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_07_27_074444) do
-
-  create_table "accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "first_name", null: false
-    t.string "family_name", null: false
-    t.string "hurigana_first", null: false
-    t.string "hurigana_family", null: false
-    t.date "birthday", null: false
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_accounts_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2020_07_27_070406) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "shipping_first_name", null: false
     t.string "shipping_family_name", null: false
+    t.string "shipping_hurigana_first", null: false
+    t.string "shipping_hurigana_family", null: false
     t.string "zipcode", null: false
-    t.string "prefecture", null: false
+    t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "address", null: false
     t.string "others"
@@ -42,17 +31,17 @@ ActiveRecord::Schema.define(version: 2020_07_27_074444) do
   end
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "brand_name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "category_name", null: false
     t.string "ancestry"
-    t.index ["ancestry"], name: "index_categories_on_ancestry"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "credits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
