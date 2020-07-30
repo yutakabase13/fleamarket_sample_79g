19cb633535e1b00c269fd8e1c09bcfa84eb160ef
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-07-30 16:38:14 +0900

    add shipping, fee and status active hash

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,3 +1,5 @@
 class Shipping < ActiveHash::Base
-  
+  self.data = [
+    {id: 1, name: '1~2日'}, {id: 2, name: '2~3日'}, {id: 3, name: '3~4日'}, {id: 4, name: '4~5日'}, {id: 5, name: '一週間以上'}, 
+  ]
 end
---
+++ app/models/status.rb
@@ -0,0 +1,5 @@
+class Status < ActiveHash::Base
+  self.data = [
+    {id: 1, name: '新品'}, {id: 2, name: '未使用に近い'}. {id: 3, name: '目立った傷等はない'}, {id: 4, name: '軽い傷がある'}, {id: 5, name: '汚れがある'}, {id: 6, name: 'かなり汚れている'}
+  ]
+end
