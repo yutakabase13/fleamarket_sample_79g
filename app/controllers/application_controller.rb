feb9b4700fb59f0043b09deef30abc6b90001f92
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-08-05 15:08:24 +0900

    chnage basic_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,8 @@ class ApplicationController < ActionController::Base
   private
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.credentials[:basic_auth][:user] &&
-      password == Rails.application.credentials[:basic_auth][:pass]
+      username == ENV["BASIC_AUTH_USER"] &&
+      password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
   def production?
